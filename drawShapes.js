47bcdf176be0292f7546d922e043e8f112889f79
mcheng3 <michael.cheng65@gmail.com>
2018-02-05 20:31:14 -0500

    Finish work 01

--- drawShapes.js
+++ drawShapes.js
@@ -15,20 +15,25 @@ var drawShape = function(e){
 	if(drawCircle){
 		console.log("x: " + e.clientX + "  y: " + e.clientY);
 		ctx.fillStyle="#A7414A";
+		ctx.lineTo(e.offsetX, e.offsetY);
+		ctx.stroke();
 		ctx.beginPath();
-		ctx.arc(e.clientX, e.clientY, 10, 0, 2*Math.PI);
-		//ctx.stroke();
+		ctx.arc(e.offsetX, e.offsetY, 10, 0, 2*Math.PI);
+		ctx.stroke();
 		ctx.fill();
+		ctx.beginPath();
+		ctx.moveTo(e.offsetX, e.offsetY);	
+		
 	}
-	else{
+	/*else{
 		ctx.fillStyle="#282726";
 		ctx.beginPath();
-		ctx.rect(e.clientX, e.clientY, 20, 20);
+		ctx.rect(e.offsetX, e.offsetY, 20, 20);
 		//ctx.stroke();
 		ctx.fill();
-	}
+	}*/
 }    
 
 document.getElementById("clear").addEventListener("click", clear);
-document.getElementById("toggle").addEventListener("click", toggle);
+//document.getElementById("toggle").addEventListener("click", toggle);
 document.getElementById("drawarea").addEventListener("click", drawShape);
